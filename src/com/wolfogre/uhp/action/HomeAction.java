cb4f6ada89d2705050403ccc1784f0272da1ec41
Jason Song <wolfogre@qq.com>
2016-07-18 01:52:42 +0800

    WTF

--- src/com/wolfogre/uhp/action/HomeAction.java
+++ src/com/wolfogre/uhp/action/HomeAction.java
@@ -5,7 +5,7 @@ import com.opensymphony.xwork2.ActionSupport;
 /**
  * Created by wolfogre on 16-7-18.
  */
-public class HomeAction extends ActionSupport {
+public class HomeAction extends ActionSupport{
     public String index(){
         return SUCCESS;
     }
