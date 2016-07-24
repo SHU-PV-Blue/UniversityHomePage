8194c1d5202e3f2cae9fdbafd46c85124162f243
Jason Song <wolfogre@qq.com>
2016-07-24 17:18:02 +0800

    Revert "Revert "WTF of the DAO""
    This reverts commit c6d9f1fcb414e8308e6aa14100a7b42cc67d695d.

--- src/com/wolfogre/uhp/action/HomeAction.java
+++ src/com/wolfogre/uhp/action/HomeAction.java
@@ -2,18 +2,57 @@ package com.wolfogre.uhp.action;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
-import com.wolfogre.common.dao.HibernateDao;
+import com.wolfogre.common.HibernateTool;
 import com.wolfogre.uhp.domain.HomePageEntity;
+import org.apache.struts2.ServletActionContext;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by wolfogre on 16-7-18.
  */
 public class HomeAction extends ActionSupport{
-    String imagePath;
+    private String admin;
 
-    public String index(){
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+    public String index() throws Exception{
         ActionContext actionContext = ActionContext.getContext();
-        actionContext.put("universityList", new HibernateDao<HomePageEntity>().findAll(HomePageEntity.class));
+        actionContext.put("universityList", new HibernateTool().getAll(HomePageEntity.class));
+        if(getAdmin() != null && "df8c608611d9139055f0ffb5cd79d0db".equals(getMD5(getAdmin())))
+            actionContext.put("admin", true);
+        else
+            actionContext.put("admin", false);
         return SUCCESS;
+    }
+
+    public String getMD5() throws Exception{
+        String jsonString = "false";
+        if(getAdmin() != null){
+            jsonString = "{\"admin\":\"" + getAdmin() +  "\", \"md5\":\"" + getMD5(getAdmin()) +  "\"}";
+        }
+
+        ServletActionContext.getResponse().getWriter().println(jsonString);
+        return null;
+    }
+
+    private String getMD5(String input) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] messageDigest = md.digest(input.getBytes());
+        BigInteger number = new BigInteger(1, messageDigest);
+        String hashtext = number.toString(16);
+        // Now we need to zero pad it if you actually want the full 32 chars.
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
     }
 }
